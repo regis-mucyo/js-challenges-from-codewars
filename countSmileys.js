5079f5f48607796931f028d7361f2c6e67aee2d6
Regis Mucyo <mucyoregis250@gmail.com>
2025-07-21 20:33:20 +0200

    finalize the challenge of count smiles in array

--- countSmileys.js
+++ countSmileys.js
@@ -1,6 +1,29 @@
 const countSmileys = (count) => {
-  console.log(count);
+  return count.filter((e) =>
+    [
+      ":)",
+      ":D",
+      ":-D",
+      ":-)",
+      ":~D",
+      ":~)",
+      ";)",
+      ";D",
+      ";-D",
+      ";-)",
+      ";~D",
+      ";~)",
+    ].includes(e)
+      ? e
+      : false
+  ).length;
 };
-countSmileys([":)", ";(", ";}", ":-D"]); // should return 2;
-countSmileys([";D", ":-(", ":-)", ";~)"]); // should return 3;
-countSmileys([";]", ":[", ";*", ":$", ";-D"]); // should return 1;
+console.log(countSmileys([":)", ";(", ";}", ":-D"])); // should return 2;
+console.log(countSmileys([";D", ":-(", ":-)", ";~)"])); // should return 3;
+console.log(countSmileys([";]", ":[", ";*", ":$", ";-D"])); // should return 1;
+console.log(countSmileys([])); //0
+console.log(countSmileys([":D", ":~)", ";~D", ":)"])); // should return 4;
+console.log(countSmileys([":)", ":(", ":D", ":O", ":;"])); // should return 2;
+console.log(countSmileys([";]", ":[", ";*", ":$", ";-D"])); // should return 1;
+// This kata is to return number of smiles we have  in our array and there's a list of those all smiles ,
+// so we was checking according to that lists of smiles
