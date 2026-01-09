06e42c089867f06fa77d42694f6631f17042a554
Regis Mucyo <mucyoregis250@gmail.com>
2026-01-09 06:55:41 +0200

    Final solution for order 6-kyu kata

--- 6-kyu/order.js
+++ 6-kyu/order.js
@@ -1,7 +1,12 @@
 const order = (words) => {
+  if (!words) return "";
   return words
     .split(" ")
-    .map((e) => e + e.split("").filter((e) => Number(e)))
-    .map((e) => e.split(" ").sort((a, b) => a[0] - b[0]));
+    .sort(
+      (a, b) =>
+        a.split("").filter((e) => Number(e)) -
+        b.split("").filter((e) => Number(e))
+    )
+    .join(" ");
 };
-console.log(order("is2 Thi1s T4est 3a"));
+console.log(order("4of Fo1r pe6ople g3ood th5e the2")); // Fo1r the2 g3ood 4of th5e pe6ople
